2faaab235236bf28d886576b25de28c6d64ccab7
十里 <okooo5km@gmail.com>
2026-04-28 02:09:45 +0000

    bm 0.1.1

--- Formula/bm.rb
+++ Formula/bm.rb
@@ -1,8 +1,8 @@
 class Bm < Formula
   desc "Render Mermaid diagrams as beautiful SVG/PNG/ASCII from the command line"
   homepage "https://github.com/okooo5km/beautiful-mermaid-cli"
-  url "https://registry.npmjs.org/beautiful-mermaid-cli/-/beautiful-mermaid-cli-0.1.0.tgz"
-  sha256 "0e6e6a5377c172a09c3c4bfa754aed6e600529b4df8b96a116f1580ae03e7bc6"
+  url "https://registry.npmjs.org/beautiful-mermaid-cli/-/beautiful-mermaid-cli-0.1.1.tgz"
+  sha256 "8e5ae5e38eb45b438c5e9c6afc9cd46cd84b101dfecc3a6b1ed155cd3763d5de"
   license "MIT"
 
   depends_on "node"
